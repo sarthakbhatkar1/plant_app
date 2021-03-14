e6b4a6b8af7dc2d20407d4ac2ccdd597a0426064
sarthakbhatkar1 <sarthakbhatkar003@gmail.com>
2021-03-14 19:59:43 +0530

    Initial commit plant app

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/constants.dart';
+
+import 'featured_plants.dart';
+import 'header_with_searchbox.dart';
+import 'recommend_plants.dart';
+import 'title_with_more_button.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // It will provie us total height  and width of our screen
+    Size size = MediaQuery.of(context).size;
+    // it enable scrolling on small device
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(title: "Recommended", press: () {}),
+          RecomendsPlants(),
+          TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding),
+        ],
+      ),
+    );
+  }
+}
